6ede97a3b93dfdf6a679a6ee920c19abacb330ca
Leonardo Sntiago De La Cruz <santiagodelacrvz@gmail.com>
2025-04-17 16:48:46 -0600

    semana 1

--- Semana 1/API_Conect.py
+++ Semana 1/API_Conect.py
@@ -2,30 +2,27 @@ import requests
 import re
 
 def validar_nombre_pokemon(nombre):
-    # Expresión regular para validar nombres de Pokémon (solo letras y números)
-    #Valida el nombre: Comprueba si la cadena nombre contiene solo letras y números, y si cumple con la estructura definida por la expresión regular.
+    # expresion regular para validar nombres de pokemon solo letras y numeros
+    # valida el nombre comprueba si la cadena nombre contiene solo letras y numeros y si cumple con la estructura definida por la expresion regular
     patron = re.compile(r'^[a-zA-Z0-9]+$')
-    #Retorno: Devuelve True si la cadena nombre coincide con la expresión regular, y False si no coincide.
+    # retorno devuelve true si la cadena nombre coincide con la expresion regular y false si no coincide
     return patron.match(nombre) is not None
 
 def obtener_datos_pokemon(nombre):
     if not validar_nombre_pokemon(nombre):
-        print("Nombre de Pokémon inválido. Por favor, ingrese un nombre válido.")
+        print("nombre de pokemon invalido por favor ingrese un nombre valido")
         return None
-    #url de la peticion a la POKEAPI
+    # url de la peticion a la pokeapi
     endpoint = "pokemon"
     url = f"https://pokeapi.co/api/v2/{endpoint}/{nombre.lower()}"
     try:
-        #Hacer la peticon con request.get
+        # hacer la peticon con request get
         respuesta = requests.get(url)
-        respuesta.raise_for_status()  # Verifica si la solicitud fue exitosa
-        # asignar la respuesta en formato json a datos que servira para hacer consultas 
+        respuesta.raise_for_status()  # verifica si la solicitud fue exitosa
+        # asignar la respuesta en formato json a datos que servira para hacer consultas
         datos = respuesta.json()
         return datos
-    #Todas las excepciones levantadas por Requests, heredan de la clase requests.exceptions.RequestException
+    # todas las excepciones levantadas por requests heredan de la clase requests exceptions requestexception
     except requests.exceptions.RequestException as e:
-        print(f"Error al hacer la petición: {e}")
+        print(f"error al hacer la peticion {e}")
         return None
-
-
-
