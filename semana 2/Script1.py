bc668c57eefde9939b058f71e242886d9883d8a3
Leonardo Sntiago De La Cruz <santiagodelacrvz@gmail.com>
2025-05-01 14:38:28 -0600

    eres

--- semana 2/Script1.py
+++ semana 2/Script1.py
@@ -4,15 +4,15 @@ import pandas as pd
 import re
 ################################## PRIMERA  PARTE  ###############################3
 
-def validar_nombre_pokemon(nombre):
+def validar_nombre_pokemon_API_Conect(nombre):
     # expresion regular para validar nombres de pokemon solo letras y numeros
     # valida el nombre comprueba si la cadena nombre contiene solo letras y numeros y si cumple con la estructura definida por la expresion regular
     patron = re.compile(r'^[a-zA-Z0-9-]+$')
     # retorno devuelve true si la cadena nombre coincide con la expresion regular y false si no coincide
     return patron.match(nombre) is not None
 
-def obtener_datos_pokemon(nombre):
-    if not validar_nombre_pokemon(nombre):
+def obtener_datos_pokemon_API_Conect(nombre):
+    if not validar_nombre_pokemon_API_Conect(nombre):
         print("nombre de pokemon invalido por favor ingrese un nombre valido")
         return None
     # url de la peticion a la pokeapi
